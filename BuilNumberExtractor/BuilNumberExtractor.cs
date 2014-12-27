bb86ea85f5e2f319dcdd866ba26c41c050b7fab1
drBolsen <al_mamistov@mail.ru>
2014-12-27 11:16:13 +0300

    Initial

---
+++ BuilNumberExtractor/BuilNumberExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilNumberExtractor
+{
+    public class BuilNumberExtractor
+    {
+        public static bool isFeatureBuild(string build)
+            {
+            return true;
+            }
+    }
+}
---
+++ BuilNumberExtractorTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuilNumberExtractorTest
+    {
+    [TestClass]
+    public class BuilNumberExtractorUnitTest
+        {
+        [TestMethod]
+        public void BuilNumberCorrectNonFeature()
+            {
+            Assert.IsFalse(BuilNumberExtractor.BuilNumberExtractor.isFeatureBuild("Test5.10.0161-Cluster123.xml asd"));
+            }
+
+        [TestMethod]
+        public void BuilNumberCorrectFeature()
+            {
+            Assert.IsTrue(BuilNumberExtractor.BuilNumberExtractor.isFeatureBuild("Test5.10.0161.0004-Cluster123.xml asd"));
+            }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void BuilNumberInCorrect()
+            {
+            BuilNumberExtractor.BuilNumberExtractor.isFeatureBuild("Test-Cluster123.xml asd");
+            }
+        }
+    }
