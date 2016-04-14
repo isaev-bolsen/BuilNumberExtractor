4098ebc8cca87bfae72495958c9022eca3932138
isaev-bolsen <al_mamistov@mail.ru>
2016-04-14 20:08:47 +0300

    OPUPENNOST 2

--- BuilNumberExtractor/BuilNumberExtractor.cs
+++ BuilNumberExtractor/BuilNumberExtractor.cs
@@ -8,9 +8,9 @@ namespace BuilNumberExtractor
     {
         public static bool isFeatureBuild(string build)
         {
-            if (!Regex.IsMatch(build, "\\D\\d{1}.\\d{2}.\\d{4}"))
+            if (!Regex.IsMatch(build, "\\D\\d{1}\\.\\d{2}\\.\\d{4}"))
                 throw new ArgumentException("String does not contain correct build number!");
-            return Regex.IsMatch(build, "\\d{1}.\\d{2}.\\d{4}.\\d{4}");
+            return Regex.IsMatch(build, "\\d{1}\\.\\d{2}\\.\\d{4}\\.\\d{4}");
         }
 
         public static KeyValuePair<int, int> INcr()
@@ -20,13 +20,17 @@ namespace BuilNumberExtractor
             return new KeyValuePair<int, int>(i, R);
         }
 
-        public static void Extract(string build) //Test5.10.0161-Cluster123.xml asd
+        public static int[] Extract(string build) //Test5.10.0161-Cluster123.xml asd
         {
-            var r = Regex.Match(build, "(?'version'\\d{1}).(?'subversion'\\d{2}).(?'build'\\d{4})");
+            var r = Regex.Match(build, "(?'version'\\d{1})\\.(?'subversion'\\d{2})\\.(?'build'\\d{4})\\.(?'subbuild'\\d{4})|(?'version'\\d{1})\\.(?'subversion'\\d{2})\\.(?'build'\\d{4})");
 
-            int version = int.Parse(r.Groups["version"].Value);//5
-            int subversion = int.Parse(r.Groups["subversion"].Value);//10
-            int buildN = int.Parse(r.Groups["build"].Value);//161
+            int version = int.Parse(r.Groups[1].Value);//5
+            int subversion = int.Parse(r.Groups[2].Value);//10
+            int buildN = int.Parse(r.Groups[3].Value);//161
+            var FBuild = r.Groups[4].Value;
+
+            if (string.IsNullOrEmpty(FBuild)) return new int[] { version, subversion, buildN };
+            else return new int[] { version, subversion, buildN, int.Parse(FBuild) };
         }
 
     }
--- BuilNumberExtractorTest/UnitTest1.cs
+++ BuilNumberExtractorTest/UnitTest1.cs
@@ -2,42 +2,44 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BuilNumberExtractorTest
-    {
+{
     [TestClass]
     public class BuilNumberExtractorUnitTest
-        {
+    {
         [TestMethod]
         public void BuilNumberCorrectNonFeature()
-            {
+        {
             Assert.IsFalse(BuilNumberExtractor.BuilNumberExtractor.isFeatureBuild("Test5.10.0161-Cluster123.xml asd"));
-            }
+        }
 
         [TestMethod]
         public void BuilNumberCorrectFeature()
-            {
+        {
             Assert.IsTrue(BuilNumberExtractor.BuilNumberExtractor.isFeatureBuild("zsdf234Test5.10.0161.0004-Cluster123.xml asd"));
-            }
+        }
 
         [TestMethod, ExpectedException(typeof(ArgumentException))]
         public void BuilNumberInCorrect()
-            {
+        {
             BuilNumberExtractor.BuilNumberExtractor.isFeatureBuild("Test-Cluster123.xml asd");
-            }
+        }
         [TestMethod, ExpectedException(typeof(ArgumentException))]
         public void BuilNumberInCorrect2()
-            {
+        {
             BuilNumberExtractor.BuilNumberExtractor.isFeatureBuild("Test-Cluster125.1032.0161.00043.xml asd");
-            }
+        }
 
         [TestMethod]
         public void INRTEST()
-            {
-            BuilNumberExtractor.BuilNumberExtractor.Extract("Test5.10.0161-Cluster123.xml asd");
+        {
+            var res1 = BuilNumberExtractor.BuilNumberExtractor.Extract("zsdf234Test5.10.0161.0004-Cluster123.xml asd");
+            Assert.AreEqual(res1.Length, 4);
+            var res2 = BuilNumberExtractor.BuilNumberExtractor.Extract("Test5.10.0161-Cluster123.xml asd");
+            Assert.AreEqual(res2.Length, 3);
 
             var res = BuilNumberExtractor.BuilNumberExtractor.INcr();
             Assert.AreEqual(4, res.Key);
             Assert.AreEqual(4, res.Value);
-            }
-
         }
     }
+}
